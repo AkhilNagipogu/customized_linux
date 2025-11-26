b0d5e1123c01e6da9037f31c2e3b982407d51216
Akhil Nagipogu <167780359+AkhilNagipogu@users.noreply.github.com>
2025-11-26 22:27:59 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -4,3 +4,5 @@ echo "customised Linux Set up"
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install gnome-shell-extensions gnome-shell-extension-browser-connector gnome-tweaks
+
+echo "Update done"
