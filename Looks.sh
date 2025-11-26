a52a81eaa8e659d8135c93636ac16b59b0579e4a
Akhil Nagipogu <167780359+AkhilNagipogu@users.noreply.github.com>
2025-11-26 23:29:08 +0530

    Create Looks.sh

---
+++ Looks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Coustomisation install"
+
+sudo apt install gnome-tweaks gnome-shell-extensions
+sudo apt install gnome-shell-extension-manager
+
+echo "Gnome extensions installed"
+
